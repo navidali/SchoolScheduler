67617f16e6934a4f3dd43b18bcc4e9057303d940
m-qu <74680860+m-qu@users.noreply.github.com>
2021-06-19 17:40:32 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from db import *
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication
+import sys
+import schedulerui
+
+
+class SchoolScheduler(QtWidgets.QMainWindow, schedulerui.Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(SchoolScheduler, self).__init__(parent)
+        self.setupUi(self)
 
 
 def main():
-    insert_student("test", 10)
-    get_students()
+    app = QApplication(sys.argv)
+    form = SchoolScheduler()
+    form.show()
+    app.exec_()
 
 
 if __name__ == '__main__':
-    db_init()
     main()
-    db_close()
