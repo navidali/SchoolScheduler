e90cb7c3fde0916c28de3800db8833207bb59049
jaberrio <jaberrio@users.noreply.github.com>
2021-07-30 17:44:19 -0400

    Second rust part

--- scheduler.rs
+++ scheduler.rs
@@ -15,18 +15,32 @@ pub fn schedule(c_p_course_id: *const i32,
                 c_s_period: *mut i32,
                 c_s_num: *mut i32){
     unsafe{
-        let mut freq: Vec<i32> = Vec::new();
+        let mut freq: [i32; 30] = [0; 30];
 
 
         let mut i: isize = 0;
 
         while i < c_p_num as isize{
 
-            //freq[*c_p_course_id.offset(i as isize) as usize] += 1;
-            println!("Hello");
+            freq[*c_p_course_id.offset(i as isize) as usize] += 1;
+
             i = i + 1;
         }
 
+
+        let class_id = 0;
+        let offset: i32 = 1000;
+
+        let mut course_index = 0;
+        for x in &freq{
+            println!("Index: {}, Freq: {} ",course_index, x);
+
+            let num_classes = (x+16)/15;
+
+
+
+            course_index += 1;
+        }
 
         *c_s_num = 101;
     }
